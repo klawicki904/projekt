39cd576b5c70b61dac9cde9fd4c28207af944ac6
klawicki904 <klawicki@mat.umk.pl>
2024-11-14 18:41:37 +0100

    ~projekt.cpp -> +bool czy_pierwsza(int n)

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
+
+bool czy_pierwsza(int n)
+{
+	if(n <= 1) return false;
+	for(int i = 2; i * i <= n; ++i)
+	{
+		if(n%i == 0) return false;
+	}
+	return true;
+}
+
 int main()
 {
 	int liczba;
 	cin>>liczba;
 	cout<<"Wczytano liczbe: " << liczba << endl;
+	cout<< "Czy liczba jest pierwsza: " << (czy_pierwsza(liczba) ? "tak" : "nie") << endl;
 	return 0;
 }
+
