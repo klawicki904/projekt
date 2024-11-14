5ba20dc1c76c30e60bea60f9a3be3272fc3eb1ee
klawicki904 <klawicki@mat.umk.pl>
2024-11-14 18:45:10 +0100

    ~projekt.cpp -> (sprawdzanie czy liczba jest dodatnia)

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+
+
 using namespace std;
 int main()
 {
 	int liczba;
 	cin>>liczba;
+	if(liczba <= 0)
+	{
+		cout<<"Podano liczbe niedodatnia. Program konczy dzialanie." << endl;
+		return EXIT_FAILURE;
+	}
 	cout<<"Wczytano liczbe: " << liczba << endl;
 	return 0;
 }
